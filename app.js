536e74e87193b6767c16572081a165ec95ef9473
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-09-09 21:02:45 +0300

    updated

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const app = express()
 app.listen(5000, () => console.log('Server running on port 5000'))
 
 app.get('/api', (req, res) => {
-    let { slack_name, track } = req.query
+    let { slack_name, track } = req.query;
 
-    let currentDate = new Date()
-    let day = currentDate.getDay()
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    try {
+        // Get the current UTC time with a +/-2 minute window
+        let currentDate = new Date();
+        let currentDateWithWindow = new Date(currentDate.getTime() - 2 * 60 * 1000);
 
-    res.json({
-        slack_name,
-        current_day: days[day],
-        utc_time: currentDate.toISOString().slice(0, -2) + 'Z',
-        track,
-        github_file_url: 'https://github.com/CABON-TECH/HNGx-stage_one_task/blob/main/app.js',
-        github_repo_url: 'https://github.com/CABON-TECH/HNGx-stage_one_task',
-        status_code: 200
-    })
-})
+        if (isNaN(currentDateWithWindow.getTime())) {
+            throw new Error('Invalid date object');
+        }
+
+        let day = currentDateWithWindow.getUTCDay();
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        res.json({
+            slack_name,
+            current_day: days[day],
+            utc_time: currentDateWithWindow.toISOString().split('.')[0] + 'Z',
+            track,
+            github_file_url: 'https://github.com/CABON-TECH/HNGx-stage_one_task/blob/main/app.js',
+            github_repo_url: 'https://github.com/CABON-TECH/HNGx-stage_one_task',
+            status_code: 200
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
